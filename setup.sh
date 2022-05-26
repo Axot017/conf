44d07c4dc0a6212656d15098a64d24145ce42787
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-05-26 22:34:28 +0200

    add symbolic link script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASE_DIR=$( dirname -- "$0"; )
+TARGET=".config"
+
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+for f in $(find . -type f -print)
+do
+	DIR=$( dirname -- "$f"; )
+	FILENAME=$( basename -- "$f"; )
+	[[ $FILENAME == "setup.sh" ]] && continue
+	[[ $DIR == *".git"* ]] && continue
+
+	TARGET_DIR="$HOME/$TARGET/$DIR"
+	TARGET_FILE="$HOME/$TARGET/$f"
+
+	mkdir -p $TARGET_DIR
+	rm -f $TARGET_FILE
+	ln -s $(get_abs_filename $f) $TARGET_FILE
+done
+
