8715d71a40d3fda84c9847eca835e01aca74052d
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2023-02-23 10:21:22 +0100

    add tmux config

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -11,7 +11,8 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<C-/>", "<cmd>lua _toggle_fish()<cr>", opts)
+	vim.keymap.set("t", "<C-t>", "<cmd>lua _toggle_fish()<cr>", opts)
+	vim.keymap.set("t", "<ESC>", "<cmd>lua _toggle_fish()<cr>", opts)
 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
