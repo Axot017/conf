37e8a77e2feb53ce4e51b266c446dd8bb5d43faa
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2023-04-03 10:39:16 +0200

    adjustments

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,22 +1,23 @@
 local status, null_ls = pcall(require, "null-ls")
 
 if not status then
-  vim.notify("null_ls not installed")
-  return
+	vim.notify("null_ls not installed")
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
-  b.formatting.stylua,
-  b.formatting.rustfmt,
-  b.formatting.dart_format,
-  b.formatting.terraform_fmt,
-  b.formatting.taplo,
-  b.code_actions.gitsigns,
+	b.formatting.stylua,
+	b.formatting.rustfmt,
+	b.formatting.dart_format,
+	b.formatting.terraform_fmt,
+	b.formatting.taplo,
+	b.formatting.yamlfmt,
+	b.code_actions.gitsigns,
 }
 
 null_ls.setup({
-  debug = true,
-  sources = sources,
+	debug = true,
+	sources = sources,
 })
