4c290bbd753112e20cce7b5f52135d9e161fba9a
Mateusz Ledwoń <mledwon@Mateuszs-MacBook-Pro.local>
2023-05-25 20:07:20 +0200

    conf updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
+    thefuck --alias | source 
 
     set fish_greeting
 
@@ -14,11 +15,5 @@ if status is-interactive
 
     neofetch
 
-    if [ -f '/Users/mledwon/google-cloud-sdk/path.fish.inc' ]; . '/Users/mledwon/google-cloud-sdk/path.fish.inc'; end
-
-
-    export GOOGLE_APPLICATION_CREDENTIALS="/Users/mledwon/gcloud.json"
-    export JAVA_HOME="/opt/homebrew/Cellar/openjdk/18.0.2"
-    thefuck --alias | source 
 end
 
